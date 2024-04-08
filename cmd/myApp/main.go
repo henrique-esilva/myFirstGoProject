fa6cb32ac37d60286b3c0f95c8da1243d9392dea
Pedro Henrique <eduardofilhojj@gmail.com>
2024-04-07 21:13:22 -0300

    can render various layers; add fall; terrain stops fall

--- cmd/myApp/main.go
+++ cmd/myApp/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
-	//"encoding/json"
+	//"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"encoding/json"
+	"io/ioutil"
 	"image"
 	"time"
 	"log"
@@ -13,14 +14,15 @@ import (
 
 const (
 	scale        = 2 // Fator de escala para a imagem
-	screenWidth  = 16*20*scale
-	screenHeight = 16*12*scale
+	screenWidth  = 320*scale
+	screenHeight = 192*scale
 )
 
 var (
 	logicTicker *time.Ticker
 	frameTicker *time.Ticker
 	animTicker *time.Ticker
+	fallTicker *time.Ticker
 	charactersImage *ebiten.Image
 	tilesImage *ebiten.Image
 	//grassTile *ebiten.Image
@@ -29,15 +31,20 @@ var (
 	king Character
 	snake Character
 	tiles [137] *ebiten.Image
-	tilemap []int
+	colisionTerrain [20][13]int
+	tilemap Tilemap
 	mapWidth int
 	mapHeight int
 	characters[] *Character
 )
 
+//type JsonLayers struct {
+//	Data []int `json:"data"`
+//}
+
 type Tilemap struct {
-	layers []struct {
-		data []int `json:"data"`
+	Layers []struct {
+		Data []int `json:"data"`
 	} `json:"layers"`
 }
 
@@ -52,6 +59,8 @@ type Character struct {
 	image *ebiten.Image
 	position [2]int
 	invert int
+	jump int
+	landed int
 }
 
 type Game struct{}
@@ -61,20 +70,25 @@ func (g *Game) Update() error {
 	case<-logicTicker.C:
 		//select{
 		//case<-frameTicker.C:
-		if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) {
-			scout.position[1]--
-		}
-		if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) {
+		/*if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		inpututil.IsKeyJustPressed(ebiten.)
 			scout.position[1]++
+		}*/
+		limitx, limity := 19, 11
+		if scout.landed == 1 {//position[1] >= limity{
+			if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+				scout.landed = 0
+				scout.jump = 3
+			}
 		}
-		if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) {
-			scout.invert = -1
-			scout.position[0]--
-		}
-		if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) {
-			scout.invert = 1
-			scout.position[0]++
-		}
+		// if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		// 	scout.invert = -1
+		// 	scout.position[0]--
+		// }
+		// if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		// 	scout.invert = 1
+		// 	scout.position[0]++
+		// }
 		//default:
 		//}
 
@@ -84,18 +98,47 @@ func (g *Game) Update() error {
 		case <-animTicker.C:
 			for _, currentCharacter := range characters{
 				currentCharacter.index++
+				if currentCharacter.index > currentCharacter.indexRange[1]{
+					currentCharacter.index = currentCharacter.indexRange[0]
+				}
 			}
 		default:
 		}
 
+		select{
+		case <-fallTicker.C:
+			if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+				scout.invert = -1
+				scout.position[0]--
+			}
+			if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+				scout.invert = 1
+				scout.position[0]++
+			}
+			for _, currentCharacter := range characters {
+				if currentCharacter.jump > 0 {
+					currentCharacter.position[1]--
+					currentCharacter.jump--
+				} else {
+					if currentCharacter.position[1] <= limity {
+						fmt.Println(colisionTerrain[scout.position[0]][scout.position[1]])
+						currentCharacter.position[1]++
+						if colisionTerrain[currentCharacter.position[0]][currentCharacter.position[1]] == 1 || currentCharacter.position[1] > limity {
+							currentCharacter.position[1]--
+							currentCharacter.landed = 1
+						} else {currentCharacter.landed = 0}
+					} else {currentCharacter.landed = 1}
+				}
+			}
+		default:
+		}
+
+		fmt.Println(scout.position[1])
 		for _, currentCharacter := range characters {
 			if currentCharacter.position[0] < 0{currentCharacter.position[0] = 0}
-			if currentCharacter.position[0] > 19{currentCharacter.position[0] = 19}
+			if currentCharacter.position[0] > limitx{currentCharacter.position[0] = limitx}
 			if currentCharacter.position[1] < 1{currentCharacter.position[1] = 1}
-			if currentCharacter.position[1] > 11{currentCharacter.position[1] = 11}
-			if currentCharacter.index > currentCharacter.indexRange[1]{
-				currentCharacter.index = currentCharacter.indexRange[0]
-			}
+			if currentCharacter.position[1] > limity{currentCharacter.position[1] = limity}
 		}
 
 		x0, y0 = scout.index*32, 0
@@ -192,7 +235,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		}
 	}*/
 
-	for ind, imgind := range tilemap{
+	/*for ind, imgind := range tilemap{
 		if imgind != 0 {
 			corr := imgind-1
 			pos := [2]int{ind%20, ind/20}
@@ -201,11 +244,25 @@ func (g *Game) Draw(screen *ebiten.Image) {
 			op.GeoM.Scale(scale, scale)
 			screen.DrawImage(tiles[corr], op)
 		}
+	}*/
+
+	for _, layer := range tilemap.Layers {
+		for ind, imgind := range layer.Data{
+			if imgind != 0 {
+				corr := imgind//-1
+				pos := [2]int{ind%20, ind/20}
+				op := &ebiten.DrawImageOptions{}
+				op.GeoM.Translate(float64(pos[0]*16), float64(pos[1]*16))
+				op.GeoM.Scale(scale, scale)
+				screen.DrawImage(tiles[corr], op)
+			}
+		}
 	}
 
 	for _, currentCharacter := range characters {
 		options := &ebiten.DrawImageOptions{}
-		x, y := float64(currentCharacter.position[0]*16)-8, float64(currentCharacter.position[1]*16-16)
+		//x, y := float64(currentCharacter.position[0]*16)-8, float64(currentCharacter.position[1]*16-16)
+		x, y := float64(currentCharacter.position[0]*16)-8, float64(currentCharacter.position[1]*16)-16
 		options.GeoM.Scale(float64(currentCharacter.invert), 1)
 		if currentCharacter.invert == -1{options.GeoM.Translate(32, 0)}
 		options.GeoM.Translate(x, y)
@@ -219,8 +276,8 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
     // Therefore there is a loss of information here.
     // fmt.Println(outsideWidth, outsideHeight)
 
-    s := ebiten.DeviceScaleFactor()
-    return int(float64(outsideWidth) * s), int(float64(outsideHeight) * s) // Will result in 300x300 with 125% UI scaling.
+    //s := ebiten.DeviceScaleFactor()
+    return int(float64(outsideWidth)), int(float64(outsideHeight)) // Will result in 300x300 with 125% UI scaling.
 }
 
 func resumeRect (x, y int) image.Rectangle {
@@ -238,28 +295,28 @@ func main() {
 		index: 0,
 		invert: 1,
 		indexRange: [2]int{0, 3},
-		position: [2]int{1, 9},
+		position: [2]int{1, 0},
 	}
 	
 	smiley = Character{
 		index: 0,
 		invert: -1,
 		indexRange: [2]int{0, 3},
-		position: [2]int{4, 2},
+		position: [2]int{4*16, 2*16},
 	}
 
 	king = Character{
 		index: 0,
 		invert: -1,
 		indexRange: [2]int{0, 3},
-		position: [2]int{8, 9},
+		position: [2]int{8*16, 9*16},
 	}
 	
 	snake = Character{
 		index: 0,
 		invert: -1,
 		indexRange: [2]int{1, 3},
-		position: [2]int{12, 7},
+		position: [2]int{12*16, 7*16},
 	}
 	
 	characters = []*Character{
@@ -295,44 +352,59 @@ func main() {
 	tilesImage, _, err = ebitenutil.NewImageFromFile("../../assets/image/sheet.png")
 	rect = image.Rect(16*8, 0, 16*9, 16)
 
-	// tiles com 136 de comprimento
-	/*for x:=0;x<16;x++{
-		for y:=0;y<7;y++{
-			tiles[x][y]=ebiten.NewImageFromImage(tilesImage.SubImage(resumeRect(x, y)))
-		}
-	}*/
+	// an array tiles holds all tile's images
 	for x := 0; x < 17; x++ {
 		for y:=0;y<8;y++{
-			tiles[y*17+x]=ebiten.NewImageFromImage(tilesImage.SubImage(resumeRect(x, y)))
+			tiles[y*17+x+1]=ebiten.NewImageFromImage(tilesImage.SubImage(resumeRect(x, y)))
 		}
 	}
-	fmt.Println("%+v\n", tiles)
 
-	tilemap=[]int{
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 49, 50, 51, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 12, 13, 0, 0, 0, 0, 0, 0, 0,
-	0, 0, 0, 0, 0, 0, 49, 50, 0, 0, 28, 25, 30, 0, 0, 0, 0, 0, 0, 0,
-	0, 11, 12, 13, 0, 11, 12, 12, 13, 0, 28, 29, 30, 0, 0, 0, 0, 0, 0, 0,
-	0, 28, 29, 30, 0, 28, 29, 25, 30, 0, 28, 29, 30, 0, 0, 0, 0, 0, 0, 0}
+	// import game map from archives
+	myJson, err := ioutil.ReadFile("../../map/meuMapa.json")
+	if err != nil {
+		fmt.Println("Erro ao ler o arquivo:", err)
+		return
+	}
+	fmt.Println(string(myJson))
 
-	// index 11 at 170th position
-	// 10, 8
+	var tt Tilemap
 
-	//var tilemap Tilemap2
-	/*err = json.Unmarshal([]byte(myJson), &tilemap)
-    if err != nil {
-        fmt.Println("Erro ao decodificar JSON:", err)
-        return
-    }*/
+	err = json.Unmarshal(myJson, &tt)
+	if err != nil {
+		fmt.Println("Erro ao decodificar o JSON:", err)
+		return
+	}
 
-	fmt.Printf("%+v\n", tilemap)
+	tilemap = tt
+
+	colisionTerrain[1][10] = 1
+	colisionTerrain[2][10] = 1
+	colisionTerrain[3][10] = 1
+	colisionTerrain[1][11] = 1
+	colisionTerrain[2][11] = 1
+	colisionTerrain[3][11] = 1
+
+	colisionTerrain[5][10] = 1
+	colisionTerrain[6][10] = 1
+	colisionTerrain[7][10] = 1
+	colisionTerrain[8][10] = 1
+	colisionTerrain[5][11] = 1
+	colisionTerrain[6][11] = 1
+	colisionTerrain[7][11] = 1
+	colisionTerrain[8][11] = 1
+
+	colisionTerrain[10][11] = 1
+	colisionTerrain[11][11] = 1
+	colisionTerrain[12][11] = 1
+	colisionTerrain[10][10] = 1
+	colisionTerrain[11][10] = 1
+	colisionTerrain[12][10] = 1
+	colisionTerrain[10][ 9] = 1
+	colisionTerrain[11][ 9] = 1
+	colisionTerrain[12][ 9] = 1
+	colisionTerrain[10][ 8] = 1
+	colisionTerrain[11][ 8] = 1
+	colisionTerrain[12][ 8] = 1
 
 	// tiles = [10][7]TilePack{}
 
@@ -346,6 +418,8 @@ func main() {
 
 	logicTicker = time.NewTicker(time.Second/60)
 	defer logicTicker.Stop()
+	fallTicker = time.NewTicker(time.Second/2)
+	defer fallTicker.Stop()
 	frameTicker = time.NewTicker(time.Second/4)
 	defer frameTicker.Stop()
 	animTicker = time.NewTicker(time.Second /8)
